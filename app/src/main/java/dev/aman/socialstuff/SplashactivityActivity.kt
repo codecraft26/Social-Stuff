b4d17e24f74a4ef3a181c1ba9ce325ddb1c73d7b
codecraft26 <codescraft26@gmail.com>
2022-02-03 09:58:04 +0530

    splash Activity

--- app/src/main/java/dev/aman/socialstuff/SplashactivityActivity.kt
+++ app/src/main/java/dev/aman/socialstuff/SplashactivityActivity.kt
@@ -3,10 +3,11 @@ package dev.aman.socialstuff
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class SplashactivityActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this,MainActivity::class.java))
+        startActivity(Intent(this,MainActivity::class.java)) // for intent
     }
 }
